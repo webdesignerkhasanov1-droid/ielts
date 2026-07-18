754baf8e6874269fa698a15c32065db2a6f3e9ac
webdesignerkhasanov1-droid <webdesignerkhasanov1@gmail.com>
2026-07-18 16:33:41 +0300

    Configure @vitejs/plugin-legacy to support older browsers and devices

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import legacy from '@vitejs/plugin-legacy'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    legacy({
+      targets: ['> 0.2%', 'not dead', 'IE 11'],
+    })
+  ],
+  build: {
+    target: 'es2015',
+  },
   server: {
     host: true,
     port: 80,
